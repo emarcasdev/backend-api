f6d31303969debc20ea22681ece110deb8a69631
emarcasdev <emarcasdev@gmail.com>
2025-01-20 18:11:20 +0100

    preuba 22

--- api/app.js
+++ api/app.js
@@ -7,7 +7,7 @@ const { MongoClient } = require('mongodb');
 // require('dotenv').config();
 
 // const middlewares = require('./middlewares');
-const api = require('./api');
+// const api = require('./api');
 
 const app = express();
 
