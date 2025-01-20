9cbe28accc006fcda67e344222c5427f0c4da9fa
emarcasdev <emarcasdev@gmail.com>
2025-01-20 17:21:36 +0100

    preuba 14

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const app = require('./app');
 
+module.exports = app; // Exporta la app para que Vercel pueda manejarla
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   /* eslint-disable no-console */
