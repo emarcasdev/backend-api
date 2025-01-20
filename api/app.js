e95ae0ccc8bc7edd7933c27735f71ddbbfa53335
emarcasdev <emarcasdev@gmail.com>
2025-01-20 21:53:41 +0100

    add cors

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // const morgan = require('morgan');
 // const helmet = require('helmet');
-// const cors = require('cors');
+const cors = require('cors');
 const { MongoClient } = require('mongodb');
 
 // require('dotenv').config();
@@ -26,7 +26,7 @@ MongoClient.connect(uri)
 
 // app.use(morgan('dev'));
 // app.use(helmet());
-// app.use(cors());
+app.use(cors());
 
 // ✅ Obtener todos los usuarios
 app.get('/api/users', async (req, res) => {
