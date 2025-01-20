b147bdd57fb0a63d8a63d29c4e539df242bf3b89
emarcasdev <emarcasdev@gmail.com>
2025-01-20 17:32:58 +0100

    preuba 16

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const app = require('./app');
 
-module.exports = app; // Exporta la app para que Vercel pueda manejarla
 
-// const port = process.env.PORT || 5000;
-// app.listen(port, () => {
-//   /* eslint-disable no-console */
-//   console.log(`Listening: http://localhost:${port}`);
-//   /* eslint-enable no-console */
-// });
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`Listening: http://localhost:${port}`);
+  /* eslint-enable no-console */
+});
